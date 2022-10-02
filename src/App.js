51bba0a38cf40bf32ad4c2f34b7e7fe1b9b444b6
ErasmoSalsano <erasmosalsano@gmail.com>
2022-10-02 06:04:55 +0200

    conditional-rendering-02

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export class App extends React.Component{
     return(
       <div>
         <Hello />
-        <Welcome name={ <strong> Sciuti </strong> }  age={ 25 } />
+        <Welcome name={ <strong> Sciuti </strong> }  age={ 20 } />
       </div>
     )
   }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,9 @@ export class Welcome extends React.Component{
     return (
       <div>
         <p>Welcome, { this.props.name }!</p>
-        { this.props.age > 18 && <Age age={ this.props.age } /> }
+        { !!this.props.age //Check if it's present (in this case to be > 18 age should already be truth-y)
+          && this.props.age > 18
+          && <Age age={ this.props.age } /> }
       </div>
     )
   }
