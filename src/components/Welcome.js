0bf8bfddbd7d9042d8e490a880048d23de3cf4ad
ErasmoSalsano <erasmosalsano@gmail.com>
2022-10-02 06:41:54 +0200

    conditional-rendering-05

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,7 +7,7 @@ export class Welcome extends React.Component{
       <div>
         <p>Welcome, {!!this.props.name && <strong> { this.props.name } </strong>}!</p>
         { !!this.props.age //Check if it's present (in this case to be > 18 age should already be truth-y)
-          && this.props.age > 18
+          // && this.props.age > 18  //Commentato per provare conditional rendering-05
           && this.props.age < 65
           && this.props.name === 'John'
           && <Age age={ this.props.age } /> }
