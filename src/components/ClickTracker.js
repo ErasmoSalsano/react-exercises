1c5d792544f2bfb54c1df36e7252065d5f22f70e
ErasmoSalsano <erasmosalsano@gmail.com>
2022-10-14 01:14:00 +0200

    events-04

--- src/components/ClickTracker.js
+++ src/components/ClickTracker.js
@@ -5,22 +5,40 @@ import { Button } from "./Button";
 export class ClickTracker extends React.Component{
 
   state = {
-    lastPressed: <></>
+    lastPressed: <></>,
+    prova: <h1>prova</h1>
   }
 
   tracker = (event) => {
-    this.setState({
-      lastPressed: event.target.innerHTML
-    })
+    let element = event.currentTarget.querySelector('img');
+    element
+    ? this.setState({
+        lastPressed: <Button content={//Manca qulacosa, non mi piace sto metodo
+          <img
+            src={element.getAttribute('src')} 
+            height={element.getAttribute('height')}
+            alt={element.getAttribute('alt')}
+          />
+        } />
+      })
+    : this.setState({
+        lastPressed: <Button content={
+          event.currentTarget.innerHTML
+        } />
+      })
   }
 
   render(){
     return(
       <div>
-        <Button clickHandler = { this.tracker } text='Uno' />
-        <Button clickHandler = { this.tracker } text='Due' />
-        <Button clickHandler = { this.tracker } text='Tre' />
-        <h1>Last clicked: { this.state.lastPressed }</h1>
+        <Button clickHandler = { this.tracker } content='Primo' />
+        <Button clickHandler = { this.tracker } content='Secondo' />
+        <Button clickHandler = { this.tracker } content='Terzo' />
+        <br />
+        <Button clickHandler = { this.tracker } content={<img src={process.env.PUBLIC_URL+"number1.png"} height= '30px' alt='Numero 1' /> } />
+        <Button clickHandler = { this.tracker } content={<img src={process.env.PUBLIC_URL+"number2.png"} height= '30px' alt='Numero 2'/> } />
+        <Button clickHandler = { this.tracker } content={<img src={process.env.PUBLIC_URL+"number3.png"} height= '30px' alt='Numero 3'/> } />
+        {<h1>Last clicked: { this.state.lastPressed }</h1>}
       </div>
     )
   }
