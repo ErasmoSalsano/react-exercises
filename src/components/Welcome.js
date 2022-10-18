57771c4b56eebd92854f15159a357ed9d8c3a27b
Erasmo Salsano <erasmosalsano@gmail.com>
2022-10-18 21:51:32 +0200

    function-Components-01

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,23 +1,16 @@
-import React from "react";
 import { Age } from "./Age";
 
-export class Welcome extends React.Component{
-  render() {
-    return (
-      <div className='welcome'/* {this.props.className} */>
-        {/* passing the prop from the component uses the class only when passed. 
-        Putting it in the element will use it everytime the component is used */}
-        <p>Welcome, {!!this.props.name && <strong> { this.props.name } </strong>}!</p>
-        { !!this.props.age //Check if it's present (in this case to be > 18 age should already be truth-y)
-          // && this.props.age > 18  //Commentato per provare conditional rendering-05
-          && this.props.age < 65
-          && this.props.name === 'John'
-          && <Age age={ this.props.age } /> }
-      </div>
-    )
-  }
-}
 
-Welcome.defaultProps = {
-  name: 'Mister X'
+export function Welcome({name='Mister X', age}){
+
+  return(
+    <div className="welcome">
+      <p>Welcome, <strong> { name } </strong>!</p>
+        { !!age //Check if it's present (in this case to be > 18 age should already be truth-y)
+          // && age > 18  //Commentato per provare conditional rendering-05
+          && age < 65
+          && name === 'John'
+          && <Age age={ age } /> }
+    </div>
+  )
 }
