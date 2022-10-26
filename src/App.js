1ef72ff34c71194486ae71e9886cef064a257c50
Erasmo Salsano <erasmosalsano@gmail.com>
2022-10-26 23:25:02 +0200

    react-router-02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { Welcome } from './components/Welcome'
+import { Counter } from './components/Counter'
 
 export function App (){
     return(
-      <Routes>
-        <Route path='/' element={<Welcome name='Patrick' />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path='/' element={<Welcome name='Patrick' />} />
+          <Route path='/counter' element={<Counter />} />
+        </Routes>
+      </div>
     )
 }
