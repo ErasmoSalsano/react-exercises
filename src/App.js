cb7da9ef9b8ab8f320d2b925faad06a42866abbc
Erasmo Salsano <erasmosalsano@gmail.com>
2022-10-27 00:25:59 +0200

    react-router-04

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Link } from 'react-router-dom'
 import { Welcome } from './components/Welcome'
 import { Counter } from './components/Counter'
 import { ShowGithubUser } from './components/ShowGithubUser'
@@ -6,10 +6,13 @@ import { ShowGithubUser } from './components/ShowGithubUser'
 export function App (){
     return(
       <div>
+        <nav><Link to='/' >Home</Link> | <Link to='/counter' >Counter</Link> | <Link to='/users/' >Users</Link></nav>
         <Routes>
           <Route path='/' element={<Welcome name='Patrick' />} />
           <Route path='/counter' element={<Counter />} />
-          <Route path='/users/:username' element={<ShowGithubUser />} />
+          <Route path='/users' element={<ShowGithubUser />} >
+            <Route path=':username' element={<ShowGithubUser />} />
+          </Route>
         </Routes>
       </div>
     )
--- src/components/ShowGithubUser.js
+++ src/components/ShowGithubUser.js
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom'
 import { GithubUser } from './GithubUser'
 
 export function ShowGithubUser(){
-  const { username = 'PatrizioDevelhope' } = useParams();
+  const { username = 'ErasmoSalsano' } = useParams();
 
   return <GithubUser userName={ username } />
 }
